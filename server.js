795668c334c78ffa225ebfe2e74f13149a08dc11
Shalom Raichman <shalomr1770@gmail.com>
2024-09-23 11:31:49 +0300

    add get by id ep

--- server.js
+++ server.js
@@ -24,8 +24,20 @@ app.get("/amn", async (req, res)=>{
     }
 })
 
-app.get("/amn/:id", (req, res)=>{
-    res.send("Function not implemented yet")
+app.get("/amn/:id", async (req, res)=>{
+    try {
+        // get data from file
+        const data = JSON.parse(await fs.readFile("./data.json", "utf-8"))
+        // find the one with the right id
+        const amn = data.find(a => a.id == req.params.id)
+        // send it back to the client
+        res.json(amn)
+    } catch (error) {
+        res.status(500).json({
+            error: true,
+            message: error
+        })
+    }
 })
 
 app.get("/amn/summary", (req, res)=>{
